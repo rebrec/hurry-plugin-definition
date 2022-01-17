b527cf40e91f02f5cb47c6ba71853a3696958cf1
rebrec <elfrancesc.o+npm@gmail.com>
2022-01-17 07:56:00 +0100

    add DS definitinos

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,5 @@
-import {Plugin as P} from './Plugin'
-export const Plugin = P
+import {Plugin as PluginImported} from '../../src/core/Plugin'
+import {DatasourceJS as DatasourceJSImported, DatasourceShell as DatasourceShellImported} from '../../src/core/Datasource/DatasourceBase'
+export const Plugin = PluginImported
+export const DatasourceJs = DatasourceJSImported
+export const DatasourceShell = DatasourceShellImported
